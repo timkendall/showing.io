a8aded25ae20a83a1d228b7b8b17b8d4edd0458b
Timothy Kendall <timkendall18@gmail.com>
2014-06-08 20:36:45 -0700

    Changed dev paths to deploy paths

--- scripts/config.js
+++ scripts/config.js
@@ -5,16 +5,16 @@ angular.module('Movies').config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
         when('/playing', {
-            templateUrl: '../views/playing.html'
+            templateUrl: 'views/playing.html'
         }).
         when('/opening', {
-            templateUrl: '../views/opening.html'
+            templateUrl: 'views/opening.html'
         }).
         when('/upcoming', {
-            templateUrl: '../views/upcoming.html'
+            templateUrl: 'views/upcoming.html'
         }).
         when('/:id', {
-            templateUrl: '../views/movie.html'
+            templateUrl: 'views/movie.html'
         }).
         otherwise({
             redirectTo: '/playing'
